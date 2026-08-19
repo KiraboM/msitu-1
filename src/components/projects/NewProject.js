8a8ed47beb64f2013de9915a15c14813f9d20f7c
KiraboM <kirabomusoke46@gmail.com>
2026-08-19 15:14:44 +0100

    Made New Project Screen able to scroll. Now just need to make sure footer appears.

--- src/components/projects/NewProject.js
+++ src/components/projects/NewProject.js
@@ -12,6 +12,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import AnimatedLoader from "react-native-animated-loader";
 import { generateProject } from '../../store/projects';
 import { convertToMeters } from '../../utils';
+import { TouchableOpacity } from 'react-native';
 
 const animation = new SlideAnimation({
     initialValue: 0,
@@ -115,6 +116,8 @@ export default function NewProject({ visible, onClose, roverLocation }) {
         <Modal
             visible={visible}
             modalAnimation={animation}
+            height={0.9}
+            width={1.0}
             modalTitle={
                 <ModalTitle
                     textStyle={styles.buttonText}
@@ -137,154 +140,168 @@ export default function NewProject({ visible, onClose, roverLocation }) {
                 </ModalFooter>
             }
         >
-            <ModalContent
-                style={{
-                    marginTop: 5,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: 500,
-                    flex: 1
-                }}
+            <ScrollView 
+                style={{flex: 1}} 
+                nestedScrollEnabled={true} 
+                scrollEnabled={true}  
+                contentContainerStyle={{flexGrow: 1}} 
+                keyboardShouldPersistTaps="handled" 
+                keyboardDismissMode="on-drag"
             >
-                <View className='p-2 w-96' style={{flex: 1, maxHeight: 500}}>
-                    <ScrollView nestedScrollEnabled={true}>
-                        <MsTextInput
-                            onChangeText={setProjectName}
-                            label="Project Name" />
-                        <View className='flex-row justify-between w-full gap-x-1 mt-3'>
-                            <MsTextInput
-                                containerStyle={{ width: 150 }}
-                                keyboardType="decimal-pad"
-                                label="Line Length"
-                                onChangeText={setLineLength}
+                    <ModalContent
+                        style={{
+                            marginTop: 5,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <View style={{flex: 1, height: 1000}}>
+                            {/* <ScrollView 
+                                style={{flex: 1}} 
+                                nestedScrollEnabled={true} 
+                                scrollEnabled={true}  
+                                contentContainerStyle={{flexGrow: 1}} 
+                                keyboardShouldPersistTaps="handled" 
+                                keyboardDismissMode="on-drag"
+                            > */}
+                                <MsTextInput
+                                    onChangeText={setProjectName}
+                                    label="Project Name" />
+                                <View className='flex-row justify-between w-full gap-x-1 mt-3'>
+                                    <MsTextInput
+                                        containerStyle={{ width: 150 }}
+                                        keyboardType="decimal-pad"
+                                        label="Line Length"
+                                        onChangeText={setLineLength}
 
-                            />
-                            <Picker
-                                style={{ width: 130, marginTop: 5 }}
-                                selectedValue={lineLengthUnit}
-                                onValueChange={(itemValue, itemIndex) =>
-                                    setLineLengthUnit(itemValue)
-                                }>
-                                <Picker.Item style={{ ...styles.textMedium }} label="Units" value="" />
-                                <Picker.Item style={{ ...styles.textMedium }} label="Feet" value="feet" />
-                                <Picker.Item style={{ ...styles.textMedium }} label="Metres" value="meter" />
-                                <Picker.Item style={{ ...styles.textMedium }} label="Acres" value="acres" />
-                                <Picker.Item style={{ ...styles.textMedium }} label="Miles" value="miles" />
-                            </Picker>
-                        </View>
-                        <View className='flex-row justify-between w-full gap-x-1 mt-3'>
-                            <MsTextInput
-                                keyboardType="decimal-pad"
-                                onChangeText={setGapSize}
-                                label="Gap Size"
-                                containerStyle={{ width: 150 }}
-                            />
-                            <Picker
-                                style={{ width: 130, marginTop: 5 }}
-                                selectedValue={gapSizeUnit}
-                                onValueChange={(itemValue, itemIndex) =>
-                                    setGapSizeUnit(itemValue)
-                                }>
-                                <Picker.Item style={{ ...styles.textMedium }} label="Units" value="" />
-                                <Picker.Item style={{ ...styles.textMedium }} label="Feet" value="feet" />
-                                <Picker.Item style={{ ...styles.textMedium }} label="Inches" value="inches" />
-                                <Picker.Item style={{ ...styles.textMedium }} label="Metres" value="meter" />
-                            </Picker>
-                        </View>
+                                    />
+                                    <Picker
+                                        style={{ width: 130, marginTop: 5 }}
+                                        selectedValue={lineLengthUnit}
+                                        onValueChange={(itemValue, itemIndex) =>
+                                            setLineLengthUnit(itemValue)
+                                        }>
+                                        <Picker.Item style={{ ...styles.textMedium }} label="Units" value="" />
+                                        <Picker.Item style={{ ...styles.textMedium }} label="Feet" value="feet" />
+                                        <Picker.Item style={{ ...styles.textMedium }} label="Metres" value="meter" />
+                                        <Picker.Item style={{ ...styles.textMedium }} label="Acres" value="acres" />
+                                        <Picker.Item style={{ ...styles.textMedium }} label="Miles" value="miles" />
+                                    </Picker>
+                                </View>
+                                <View className='flex-row justify-between w-full gap-x-1 mt-3'>
+                                    <MsTextInput
+                                        keyboardType="decimal-pad"
+                                        onChangeText={setGapSize}
+                                        label="Gap Size"
+                                        containerStyle={{ width: 150 }}
+                                    />
+                                    <Picker
+                                        style={{ width: 130, marginTop: 5 }}
+                                        selectedValue={gapSizeUnit}
+                                        onValueChange={(itemValue, itemIndex) =>
+                                            setGapSizeUnit(itemValue)
+                                        }>
+                                        <Picker.Item style={{ ...styles.textMedium }} label="Units" value="" />
+                                        <Picker.Item style={{ ...styles.textMedium }} label="Feet" value="feet" />
+                                        <Picker.Item style={{ ...styles.textMedium }} label="Inches" value="inches" />
+                                        <Picker.Item style={{ ...styles.textMedium }} label="Metres" value="meter" />
+                                    </Picker>
+                                </View>
 
-                        <View className='flex flex-col items-center mt-4'>
-                            <View className='flex flex-row justify-between gap-5'>
-                                <Chip
-                                    textStyle={styles.buttonText}
-                                    selectedColor={meshType === "TRIANGLE" ? 'green' : 'black'}
-                                    icon={() => (<Icon name={meshType === "TRIANGLE" ? `triangle` : `triangle-outline`} color={meshType === "TRIANGLE" ? 'green' : 'black'}
-                                        size={16} />)}
-                                    onPress={() => setMeshType("TRIANGLE")}
-                                >Triangular Grid</Chip>
+                                <View className='flex flex-col items-center mt-4'>
+                                    <View className='flex flex-row justify-between gap-5'>
+                                        <Chip
+                                            textStyle={styles.buttonText}
+                                            selectedColor={meshType === "TRIANGLE" ? 'green' : 'black'}
+                                            icon={() => (<Icon name={meshType === "TRIANGLE" ? `triangle` : `triangle-outline`} color={meshType === "TRIANGLE" ? 'green' : 'black'}
+                                                size={16} />)}
+                                            onPress={() => setMeshType("TRIANGLE")}
+                                        >Triangular Grid</Chip>
 
-                                <Chip
-                                    textStyle={styles.buttonText}
-                                    selected={false}
-                                    selectedColor={meshType === "SQUARE" ? 'green' : 'black'}
-                                    onPress={() => setMeshType("SQUARE")}
-                                    icon={() => (<Icon name={meshType === "SQUARE" ? `square` : `square-outline`} color={meshType === "SQUARE" ? 'green' : 'black'}
-                                        size={16} />)}
-                                >Square Grid</Chip>
-                            </View>
-                            {
-                                meshType.length > 0 &&
-                                <View className='justify-center items-center h-40 w-80 mt-2 rounded'>
-                                    {meshType === "TRIANGLE" ?
-                                        <Image
-                                            resizeMode='contain'
-                                            source={require('../../assets/tmesh.png')}
-                                            className="h-32 w-72 rounded"
-                                        />
-                                        :
-                                        <Image
-                                            resizeMode='contain'
-                                            source={require('../../assets/mmesh.png')}
-                                            className="h-32 w-72 rounded"
-                                        />
+                                        <Chip
+                                            textStyle={styles.buttonText}
+                                            selected={false}
+                                            selectedColor={meshType === "SQUARE" ? 'green' : 'black'}
+                                            onPress={() => setMeshType("SQUARE")}
+                                            icon={() => (<Icon name={meshType === "SQUARE" ? `square` : `square-outline`} color={meshType === "SQUARE" ? 'green' : 'black'}
+                                                size={16} />)}
+                                        >Square Grid</Chip>
+                                    </View>
+                                    {
+                                        meshType.length > 0 &&
+                                        <View className='justify-center items-center h-40 w-80 mt-2 rounded'>
+                                            {meshType === "TRIANGLE" ?
+                                                <Image
+                                                    resizeMode='contain'
+                                                    source={require('../../assets/tmesh.png')}
+                                                    className="h-32 w-72 rounded"
+                                                />
+                                                :
+                                                <Image
+                                                    resizeMode='contain'
+                                                    source={require('../../assets/mmesh.png')}
+                                                    className="h-32 w-72 rounded"
+                                                />
+                                            }
+                                        </View>
                                     }
                                 </View>
-                            }
-                        </View>
-                        <View className='w-full mt-2 border-b mx-2 border-teal-900'>
-                            <Picker style={{ width: 320, border: 1 }}
-                                selectedValue={gapSizeUnit}
-                                onValueChange={(itemValue, itemIndex) =>
-                                    setLineDirection(itemValue)
-                                }>
-                                <Picker.Item style={{ ...styles.textMedium }} label="Line Draw Direction" value="" />
-                                <Picker.Item style={{ ...styles.textMedium }} label="Left" value="RIGHT" />
-                                <Picker.Item style={{ ...styles.textMedium }} label="Right" value="LEFT" />
-                            </Picker>
-                        </View>
-                        <View className='w-full mt-3'>
-                            <MsCheckbox
-                                uncheckedColor='gray'
-                                disabled={true}
-                                label='First Base Point (check to auto fill)'
-                                color='green'
-                                status={checkedFirstPoint ? 'checked' : 'unchecked'}
-                                onPress={() => {
-                                    setCheckedFirstPoint(!checkedFirstPoint);
-                                    const isActive = !checkedFirstPoint;
-                                }}
-                            />
-                            <MsCheckbox
-                                uncheckedColor='gray'
-                                disabled
-                                label='Second Base Point (check to auto fill)'
-                                color='green'
-                                status={checkedSecondPoint ? 'checked' : 'unchecked'}
-                                onPress={() => {
-                                    setCheckedSecondPoint(!checkedSecondPoint);
-                                }}
-                            />
-                            {
-                                (roverLocation === null)
-                                &&
-                                <Text className='mx-3 text-xs font-avenirBold text-yellow-600'>Make sure rover is connected..</Text>
-                            }
-                            {
-                                basePoints.length > 0 &&
-                                <Text className='mx-3 text-xs font-avenirBold text-yellow-600'>{JSON.stringify(basePoints)}</Text>
-                            }
+                                <View className='w-full mt-2 border-b mx-2 border-teal-900'>
+                                    <Picker style={{ width: 320, border: 1 }}
+                                        selectedValue={gapSizeUnit}
+                                        onValueChange={(itemValue, itemIndex) =>
+                                            setLineDirection(itemValue)
+                                        }>
+                                        <Picker.Item style={{ ...styles.textMedium }} label="Line Draw Direction" value="" />
+                                        <Picker.Item style={{ ...styles.textMedium }} label="Left" value="RIGHT" />
+                                        <Picker.Item style={{ ...styles.textMedium }} label="Right" value="LEFT" />
+                                    </Picker>
+                                </View>
+                                <View className='w-full mt-3'>
+                                    <MsCheckbox
+                                        uncheckedColor='gray'
+                                        disabled={true}
+                                        label='First Base Point (check to auto fill)'
+                                        color='green'
+                                        status={checkedFirstPoint ? 'checked' : 'unchecked'}
+                                        onPress={() => {
+                                            setCheckedFirstPoint(!checkedFirstPoint);
+                                            const isActive = !checkedFirstPoint;
+                                        }}
+                                    />
+                                    <MsCheckbox
+                                        uncheckedColor='gray'
+                                        disabled
+                                        label='Second Base Point (check to auto fill)'
+                                        color='green'
+                                        status={checkedSecondPoint ? 'checked' : 'unchecked'}
+                                        onPress={() => {
+                                            setCheckedSecondPoint(!checkedSecondPoint);
+                                        }}
+                                    />
+                                    {
+                                        (roverLocation === null)
+                                        &&
+                                        <Text className='mx-3 text-xs font-avenirBold text-yellow-600'>Make sure rover is connected..</Text>
+                                    }
+                                    {
+                                        basePoints.length > 0 &&
+                                        <Text className='mx-3 text-xs font-avenirBold text-yellow-600'>{JSON.stringify(basePoints)}</Text>
+                                    }
 
+                                </View>
+                            {/* </ScrollView> */}
                         </View>
-                    </ScrollView>
-                    <AnimatedLoader
-                        visible={generating}
-                        overlayColor="rgba(255,255,255,0.75)"
-                        animationStyle={styles.lottie}
-                        animationType="slide"
-                        speed={1}>
-                        <Text className="font-avenirMedium">Generating Mesh...</Text>
-                    </AnimatedLoader>
-                </View>
-            </ModalContent>
+                        <AnimatedLoader
+                            visible={generating}
+                            overlayColor="rgba(255,255,255,0.75)"
+                            animationStyle={styles.lottie}
+                            animationType="slide"
+                            speed={1}>
+                            <Text className="font-avenirMedium">Generating Mesh...</Text>
+                        </AnimatedLoader>
+                </ModalContent>
+            </ScrollView>
         </Modal>
     )
 }
